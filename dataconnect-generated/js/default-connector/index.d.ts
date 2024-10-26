113baadf4e0bddfd109067547b74003aec629725
brandon <chefbc2k@gmail.com>
2024-10-26 06:27:44 -0500

    got a smaller schema to work !

--- dataconnect-generated/js/default-connector/index.d.ts
+++ dataconnect-generated/js/default-connector/index.d.ts
@@ -11,5 +11,26 @@ export type DateString = string;
 
 
 
+export interface NFT_Key {
+  id: UUIDString;
+  __typename?: 'NFT_Key';
+}
+
+export interface Ownership_Key {
+  nftId: UUIDString;
+  ownerId: UUIDString;
+  __typename?: 'Ownership_Key';
+}
+
+export interface Transaction_Key {
+  id: UUIDString;
+  __typename?: 'Transaction_Key';
+}
+
+export interface User_Key {
+  id: UUIDString;
+  __typename?: 'User_Key';
+}
+
 
 
