d967579129a988c7f901e7faacba9c8a9ff055bb
brandon <chefbc2k@gmail.com>
2024-10-28 20:36:02 -0500

    tried data connect ended  up using supabase

--- dataconnect/connector/dataconnect-generated/js/default-connector/esm/index.esm.js
+++
@@ -1,8 +0,0 @@
-import { getDataConnect, validateArgs } from 'firebase/data-connect';
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'speak-website-data-connect-service',
-  location: 'us-central1'
-};
-
--- dataconnect/connector/dataconnect-generated/js/default-connector/index.cjs.js
+++
@@ -1,9 +0,0 @@
-const { getDataConnect, validateArgs } = require('firebase/data-connect');
-
-const connectorConfig = {
-  connector: 'default',
-  service: 'speak-website-data-connect-service',
-  location: 'us-central1'
-};
-exports.connectorConfig = connectorConfig;
-
